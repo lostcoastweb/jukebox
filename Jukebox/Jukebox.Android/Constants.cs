6d2889899d40e94f644884c4c8623dbf2c621bdc
Adam Carter <mail@adam-carter.com>
2020-01-30 10:14:57 -0800

    Code is more or less in sandbox mode.

---
+++ Jukebox/Jukebox.Android/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Jukebox.Droid
+{
+    class Constants
+    {
+        static string DefaultMusicPath = "/storage/emulated/0/Music";
+    }
+}
