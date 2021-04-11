b621a30f40f974a454b2dce0677830d6e7329965
dmb851 <46007029+dbjerr@users.noreply.github.com>
2021-04-10 20:08:16 -0700

    added buttons and icons for volume up/down/mute on react side

--- jukebox.webplayer/src/views/musicplayer/MusicPlayer.js
+++ jukebox.webplayer/src/views/musicplayer/MusicPlayer.js
@@ -68,9 +68,41 @@ const axios = require('axios').default;
     console.log("pressed prev");
 
 }
+ export function volDown(event){
+     event.preventDefault();
+
+     axios.get(config.Routes.volDown)
+        .then((response) => {
+           console.log(response.data + "volDown")
+         }
+      ).catch(function (error){console.log("error volDown")});
+    console.log("pressed volDown");
+
+}
+ export function volUp(event){
+     event.preventDefault();
+
+     axios.get(config.Routes.volUp)
+        .then((response) => {
+           console.log(response.data + "volUp")
+         }
+      ).catch(function (error){console.log("error volUp")});
+    console.log("pressed volUp");
+
+}
+ export function mute(event){
+     event.preventDefault();
+
+     axios.get(config.Routes.mute)
+        .then((response) => {
+           console.log(response.data + "mute")
+         }
+      ).catch(function (error){console.log("error mute")});
+    console.log("pressed mute");
+
+}
 
 function MusicPlayer() {
-
 
     return(
         <>
@@ -87,14 +119,20 @@ function MusicPlayer() {
 
                 {/* Music Player Buttons */}
                 <Row className="justify-content-center m-5">
-
-                        <Button className="m-3 prevBtn playerBtn"  onClick={e=>playPrev(e)} id="prevBtn" ></Button>
+                      <Button className="m-3 prevBtn playerBtn"  onClick={e=>playPrev(e)} id="prevBtn" ></Button>
                    
                         <Button className="m-3 pauseBtn hidden playerBtn" alt="pause" onClick={e=>pause(e)} id="pauseBtn"></Button>
                       
                         <Button className="m-3 playBtn playerBtn" alt="play" id="playBtn" onClick={e=>play(e)}></Button>
                     
                         <Button className="m-3 nextBtn playerBtn"  onClick={e=>playNext(e)} id="nextBtn"></Button>
+                      
+
+                      
+                        <Button className="m-3 volDownBtn playerBtn"  onClick={e=>volDown(e)} id="volDown"></Button>
+                        <Button className="m-3 volUpBtn playerBtn"  onClick={e=>volUp(e)} id="volDown"></Button>
+                        <Button className="m-3 muteBtn playerBtn"  onClick={e=>mute(e)} id="mute"></Button>  
+                        
                 
                 </Row>
            
