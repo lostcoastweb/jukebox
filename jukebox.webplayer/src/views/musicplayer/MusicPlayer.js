aeb83d23d49dfcfbef22b8603b4f9ca1a22d8619
Jeff Rasnic <jcr126@humboldt.edu>
2021-04-13 13:12:11 -0700

    Basic playlist implemented into React front-end.

--- jukebox.webplayer/src/views/musicplayer/MusicPlayer.js
+++ jukebox.webplayer/src/views/musicplayer/MusicPlayer.js
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import React from 'react';
+
 import '../../App.css';
 const axios = require('axios').default;
 
@@ -23,7 +25,34 @@ const axios = require('axios').default;
 
 }
 
+// Using this format for function, is this incorrect?
+const nextPlaylist = (setPlaylist) => {
+    axios.get('http://localhost:8080/api/media/playlist/next')
+        .then((response) => {
+            console.log("Playlist " + response.data + " started.")
+            setPlaylist(response.data);
+        }).catch((error) => { console.log("Failed to start next playlist.") });
+};
+
+const previousPlaylist = (setPlaylist) => {
+    axios.get('http://localhost:8080/api/media/playlist/previous')
+        .then((response) => {
+            console.log("Playlist " + response.data + " started.")
+            setPlaylist(response.data);
+        }).catch((error) => { console.log("Failed to start previous playlist.") });
+};
+
 function MusicPlayer() {
+
+    const [playlist, setPlaylist] = React.useState({})
+
+    const handleNext = () => {
+        nextPlaylist(setPlaylist);
+    };
+
+    const handlePrev = () => {
+        previousPlaylist(setPlaylist);
+    };
 
     return(
         <>
@@ -50,8 +79,23 @@ function MusicPlayer() {
                         <Button className="m-3 nextBtn playerBtn"></Button>
                 
                 </Row>
-           
 
+                
+
+        </Container>
+
+        {/* Playlist section */}
+        <Container className="centered player-border mt-5">
+            <Row className="justify-content-center m-5">
+                { playlist !== {} ? (<div>Playlist - None</div>) : (<div>Playlist - {playlist.title}</div>) }
+            </Row>
+
+            <Row className="justify-content-center m-5">
+
+                <Button className="m-3 prevBtn playerBtn" onClick={handlePrev}></Button>
+                <Button className="m-3 nextBtn playerBtn" onClick={handleNext}></Button>
+
+            </Row>
         </Container>
 
         </>
